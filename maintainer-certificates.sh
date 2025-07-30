d8a5742b044c2d542db90f58332b694d7f599ea9
Maarten Menheere <m.menheere@m2tec.nl>
2025-07-30 16:21:16 +0200

    Add graphql certificate query

--- maintainer-certificates.sh
+++ maintainer-certificates.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+set -x 
+
+curl 'https://mainnet-moonflower.m2tec.nl/cardano-graphql' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: https://mainnet-moonflower.m2tec.nl' --data-binary '{"query":"query GetUtxosByPolicyId($policyId: Hash28Hex!) {\n  utxos(where: { tokens: { asset: { policyId: { _eq: $policyId } } } }) {\n    address\n  }\n}","variables":{"policyId":"56dc578b51e07227fab63051fb72366d7d8e10e46d17f26090db9280"}}' --compressed
